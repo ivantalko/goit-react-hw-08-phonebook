f165e4d2a631db901fa2b61c41685e73c6988238
ivantalko <108145743+ivantalko@users.noreply.github.com>
2022-11-04 14:10:08 +0200

    home 6 new

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,22 +2,26 @@ import { ContactForm } from './ContactForm/ContactForm';
 import { ContactList } from './ContactList/ContactList';
 import { FilterContacts } from './FilterContacts/FilterContacts';
 import { nanoid } from 'nanoid';
-import { useState } from 'react';
 import { useEffect } from 'react';
-// import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import {
+  addContactAction,
+  changeFilterAction,
+  deleteContactAction,
+} from 'redux/Phonebook.redux/actionList';
 // import { addContact } from 'redux/Phonebook.redux/typseactions';
 
 export const App = () => {
-  // const dispatch = useDispatch();
-  const [filter, setFilter] = useState('');
-  // const contacts = useSelector(state => state.contacts);
-  const [contacts, setContacts] = useState(() => {
-    const getContact = localStorage.getItem('contacts');
-    if (getContact) {
-      return JSON.parse(getContact);
-    }
-    return [];
-  });
+  // const [filter, setFilter] = useState('');
+  const contacts = useSelector(state => state.contacts);
+  const filter = useSelector(state => state.filter);
+  // const [contacts, setContacts] = useState(() => {
+  //   const getContact = localStorage.getItem('contacts');
+  //   if (getContact) {
+  //     return JSON.parse(getContact);
+  //   }
+  //   return [];
+  // });
   // useEffect(() => {
   //   const getContact = localStorage.getItem('contacts');
   //   if (getContact) {
@@ -27,29 +31,28 @@ export const App = () => {
   useEffect(() => {
     localStorage.setItem('contacts', JSON.stringify(contacts));
   }, [contacts]);
-
+  const dispatch = useDispatch();
   const handleDeleteUser = id => {
-    setContacts(contacts => {
-      return contacts.filter(item => item.id !== id);
-    });
+    dispatch(deleteContactAction(id));
   };
+
   const getFilteredContacts = () => {
     return contacts.filter(elem =>
       elem.name.toLowerCase().includes(filter.toLowerCase())
     );
   };
   const onChangeName = event => {
-    setFilter(event.target.value);
+    dispatch(changeFilterAction(event.target.value));
+    // setFilter(event.target.value);
   };
 
   const handleSubmit = (name, number) => {
     if (contacts.some(contact => contact.name === name)) {
       return alert(`${name} is already in contacts`);
     }
-    //   dispatch(addContact({ name, number, id: nanoid() }));
-    setContacts(prevContacts => {
-      return [...prevContacts, { name, number, id: nanoid() }];
-    });
+    dispatch(addContactAction({ name, number, id: nanoid() }));
+    // setContacts(prevContacts => {
+    //   return [...prevContacts, { name, number, id: nanoid() }];
   };
 
   return (
