f715de056014f948259f994ba567952c25e01f60
ivantalko <108145743+ivantalko@users.noreply.github.com>
2022-11-11 17:18:20 +0200

    mega css

--- src/components/UserMenu/UserMenu.jsx
+++ src/components/UserMenu/UserMenu.jsx
@@ -1,6 +1,6 @@
 import { useDispatch, useSelector } from 'react-redux';
 import { logoutOperation } from 'redux/Autorization/operation.R.A';
-
+import css from './UserMenu.modele.css';
 function UserMenu() {
   const name = useSelector(state => state.registr.user?.name);
   const email = useSelector(state => state.registr.user?.email);
@@ -8,7 +8,7 @@ function UserMenu() {
 
   return (
     <>
-      <ul>
+      <ul className={css.link}>
         <li>
           <p>Hello {name}</p>
           <p>{email}</p>
